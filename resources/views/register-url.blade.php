3b3f1225ec61e3258b4e3681468e2fe46bc0d75f
Patrick Mutwiri <patwiri@gmail.com>
2023-01-07 22:37:36 +0300

    csrf token check

--- resources/views/register-url.blade.php
+++ resources/views/register-url.blade.php
@@ -7,7 +7,7 @@
         <main>
             <div id="register-url" class="container pesapal register-url">
                 <form action="{{ route('pesapal.ipn.register') }}" method="post" class="col-12">
-                    @csrf
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     <div class="form-group row">
                         <label for="ipn_url" class="col-md-4 col-form-label text-md-right">@lang('URL')</label>
 
